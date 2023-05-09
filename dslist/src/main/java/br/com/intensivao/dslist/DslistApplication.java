52eebe385e9ce08000fcface6060a7afaca33795
alifi3988 <alifi3988@gmail.com>
2023-05-09 13:48:13 -0300

    Atualizacao v.2
    Atualização do projeto v.2
     - Propertires
    - Game
    - ORM

--- dslist/src/main/java/br/com/intensivao/dslist/DslistApplication.java
+++ dslist/src/main/java/br/com/intensivao/dslist/DslistApplication.java
@@ -2,8 +2,10 @@ package br.com.intensivao.dslist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "br.com.intensivao.dslist.entities")
 public class DslistApplication {
 
 	public static void main(String[] args) {
