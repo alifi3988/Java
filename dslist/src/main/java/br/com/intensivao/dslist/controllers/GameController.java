8e8df14ecaa1d48980cc3dbd3240efcad223163b
alifi3988 <alifi3988@gmail.com>
2023-05-11 13:57:32 -0300

    Atualização v.7
    Atualização para inserir a busca pelo ID no GameController (FindById).

--- dslist/src/main/java/br/com/intensivao/dslist/controllers/GameController.java
+++ dslist/src/main/java/br/com/intensivao/dslist/controllers/GameController.java
@@ -4,14 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import br.com.intensivao.dslist.dto.GameDto;
 import br.com.intensivao.dslist.dto.GameMinDto;
 import br.com.intensivao.dslist.services.GameService;
 
 @RestController
-@RequestMapping("/games")
+@RequestMapping(value = "/games")
 public class GameController {
 	
 	
@@ -21,8 +22,15 @@ public class GameController {
 	//buscando todos os dados da table
 	@GetMapping
 	public List<GameMinDto> findAll(){
-		//realizando a conversão e retornando do tipo GameMinDto
-		return service.findAll().stream().map(x -> new GameMinDto(x)).toList();
+		return service.findAll();
 	}
+	
+	///buscar dado da tabela pelo ID
+	@GetMapping(value = "/{id}")
+	public GameDto findById(@PathVariable Long id){
+		var result = service.findById(id);
+		return result;
+	}
+	
 
 }
--- dslist/src/main/java/br/com/intensivao/dslist/services/GameService.java
+++ dslist/src/main/java/br/com/intensivao/dslist/services/GameService.java
@@ -1,12 +1,13 @@
 package br.com.intensivao.dslist.services;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import br.com.intensivao.dslist.entities.Game;
+import br.com.intensivao.dslist.dto.GameDto;
+import br.com.intensivao.dslist.dto.GameMinDto;
 import br.com.intensivao.dslist.repositorys.GameRepository;
+import jakarta.transaction.Transactional;
+
 
 @Service
 public class GameService {
@@ -15,8 +16,16 @@ public class GameService {
 	private GameRepository repository;
 	
 	
-	public List<Game> findAll(){
-		return repository.findAll();		
+	public List<GameMinDto> findAll(){
+		return repository.findAll().stream().map(x -> new GameMinDto(x)).toList();		
+	}
+	
+	@Transactional
+	public GameDto findById(Long id) {
+		System.out.println("ID: " + id);
+		var game = repository.findById(id).get();
+		System.out.println(game.toString());
+		return new GameDto(game);
 	}
 	
 }
